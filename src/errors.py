01552435836a74daf871ed921c05a14b0784968f
Kyle Stewart <kyle.w.stewart@mac.com>
2020-08-13 18:26:11 -0400

    extended error handlng coverage

--- src/errors.py
+++ src/errors.py
@@ -36,6 +36,46 @@ def refresh(error, context, config, **kwargs):
         unkown_error(error, context)
 
 
+def fee(error, context, config, **kwargs):
+    # Connection Timeout Error
+    if isinstance(error, requests.exceptions.ConnectTimeout):
+        raise click.UsageError(
+            'API connection timeout, please try again later', context)
+    # API status_code NOT 200, PEM not present in JSON file
+    elif isinstance(error, AssertionError):
+        raise click.UsageError(error.args[0], context)
+    # File not JSON
+    elif isinstance(error, json.decoder.JSONDecodeError):
+        raise click.UsageError(
+            'Incorrect file, please provide a JSON formatted addr file created by piggy addr', context)
+    else:
+        unkown_error(error, context)
+
+
+def tx(error, context, config, **kwargs):
+    # Connection Timeout Error
+    if isinstance(error, requests.exceptions.ConnectTimeout):
+        raise click.UsageError(
+            'API connection timeout, please try again later', context)
+    # API status_code NOT 200
+    elif isinstance(error, AssertionError):
+        raise click.UsageError(error.args[0], context)
+    else:
+        unkown_error(error, context)
+
+
+def signed(error, context, config, **kwargs):
+    # Connection Timeout Error
+    if isinstance(error, requests.exceptions.ConnectTimeout):
+        raise click.UsageError(
+            'API connection timeout, please try again later', context)
+    # API status_code NOT 200
+    elif isinstance(error, AssertionError):
+        raise click.UsageError(error.args[0], context)
+    else:
+        unkown_error(error, context)
+
+
 def unkown_error(error, context):
     file_name = error.__traceback__.tb_frame.f_code.co_filename
     line_num = error.__traceback__.tb_lineno
